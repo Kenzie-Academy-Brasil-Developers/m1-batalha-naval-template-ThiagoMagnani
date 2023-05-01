eace621d232ea3ba17a57cd9399921d45d76965b
Thiago <thiagoarques2@gmail.com>
2023-05-01 01:01:59 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -10,3 +10,43 @@ const seaBoard = [
 /*
   Desenvolva seu código logo abaixo
 */
+
+function allocateShips(shipPositions, initialBoard) {
+
+  for (i = 0; i < shipPositions.length; i++){
+    initialBoard[shipPositions[i][0]][shipPositions[i][1]] = "S";
+  }
+
+return initialBoard;
+
+}
+
+function checkGuesses(guesses, mountedBoard){
+
+  for (i = 0; i < guesses.length; i++){
+    if(mountedBoard[guesses[i][0]][guesses[i][1]] === "S"){
+      mountedBoard[guesses[i][0]][guesses[i][1]] = "X";
+    }
+  }
+  
+return mountedBoard;
+
+}
+
+function checkWinCondition(guessesBoard){
+
+  for (i = 0; i < guessesBoard.length; i++){
+    for (j = 0; j < guessesBoard[i].length; j++){
+      if (guessesBoard[i][j] === "S"){
+        return false;
+      }
+    } 
+  }
+
+ return true;
+
+}
+
+allocateShips();
+checkGuesses();
+checkWinCondition();
